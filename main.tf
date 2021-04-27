4974d30dcf1744f0548e49abab3258bf5173f462
saviprakash87 <rober.rajiv@gmail.com>
2021-04-26 22:19:59 -0700

    changes to add new file

--- main.tf
+++ main.tf
@@ -1,27 +1,12 @@
-provider "aws" {
-    region = "us-east-1"
-}
-
 resource "aws_instance" "myec2" {
   ami = var.ami_id
   count = var.instance_count
   instance_type = "t2.micro"
   tags = {
-    "Name" = "RAJEEV-EC2"
+    "Name" = var.ec2_tags[count.index]
   }  
 }
 
-variable "ami_id"{
-  default = "ami-0742b4e673072066f"
-  description = "AMI Id for the EC2 Instance"
-  type = string
-}
-
-variable "instance_count"{
-  default = 2
-  type = number
-description = "No of EC2 instances to create"
-}
 
 output "myec2_instance_ip"{
   value = aws_instance.myec2[*].public_ip
